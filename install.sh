bc468787bee5634f79d4e49c633fa580c66381e9
William <xiningbank@gmail.com>
2020-12-08 22:48:28 +0800

    improve and fix.

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 
 cat .bashrc >> ~/.bashrc
 
-apt-get update
-apt-get install git -y
-
 baseDir="/var/www"
 mkdir -p $baseDir
 cd $baseDir
@@ -13,14 +10,18 @@ git clone -b develop git@github.com:xiningbank/dockerlemp.git
 cd $baseDir"/dockerlemp"
 cp .env.example .env
 
+apt-get update
 apt-get install curl -y
 # for other versions, see: https://github.com/docker/compose/releases
 dcVersion="1.27.4"
 curl -L "https://github.com/docker/compose/releases/download/"$dcVersion"/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+apt-get update
 apt-get install docker.io -y
-cat ../daemon.json >> /etc/docker/daemon.json
+cd $baseDir"/autoenv"
+cat daemon.json >> /etc/docker/daemon.json
 systemctl restart docker
 
+cd $baseDir"/dockerlemp"
 docker-compose up -d --build
