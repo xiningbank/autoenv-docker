3af993ebe6a1f90497d3aa96d9af24334ef321d0
william <xiningbank@gmail.com>
2021-09-19 15:10:56 +0800

    fix error on loading .env file.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # load configrations
 cp .env.example .env
+. .env
 
 # update system
 apt-get update
@@ -17,7 +18,7 @@ source ~/.bashrc
 # install docker-compose
 # for other versions, see: https://github.com/docker/compose/releases
 apt-get install curl -y
-curl -L "https://github.com/docker/compose/releases/download/"${DOCKER_COMPOSE_VERSION}"/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # download docker LEMP
