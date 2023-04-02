06b960b439898a20264dcb97e1d91eda805566f6
william <xiningbank@gmail.com>
2023-04-02 12:36:08 +0800

    1.remove the exit for dockerlemp; 2.recover docker compose version to 1.29.2 in .env.example; 3.set dockerlemp git branch to master in .env.example

--- install.sh
+++ install.sh
@@ -23,8 +23,6 @@ cp .env.example .env
 curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-exit 0
-
 # download docker LEMP
 # for other git branch, see: https://github.com/xiningbank/dockerlemp
 baseDir="/var/www"
