5e89dc0507a78fff5fbbe3ea701fb9be1b034b7f
william <xiningbank@gmail.com>
2026-02-25 23:16:57 +0800

    20260225-sudo: first commit - add sudo for commands.

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ sudo apt-get update
 # check if cURL is installed
 if ! command -v curl > /dev/null 2>&1; then
   # install cURL
-  apt-get install curl -y
+  sudo apt-get install curl -y
 fi
 
 # install docker on ubuntu, see: https://docs.docker.com/engine/install/ubuntu
-apt-get install docker.io -y
+sudo apt-get install docker.io -y
 
 # add docker repository for China
-# cat daemon.json >> /etc/docker/daemon.json
-systemctl restart docker
+#cat daemon.json | sudo tee /etc/docker/daemon.json
+sudo systemctl restart docker
 
 # add alias as command for accessing docker container.
 cat .bashrc >> ~/.bashrc
@@ -24,8 +24,8 @@ source ~/.bashrc
 # for other versions, see: https://github.com/docker/compose/releases
 # set configurations
 if [ ! -f .env ]; then
-  cp .env.example .env
+  sudo cp .env.example .env
 fi
 . .env
-curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
